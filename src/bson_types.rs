7f36064ffe3e859c5da8406a169696ab47b7ab69
Jed Estep <aje@jhu.edu>
2013-06-18 10:30:14 -0400

    implementations of Encoder and Encodable

--- src/bson_types.rs
+++ src/bson_types.rs
@@ -2,14 +2,21 @@
 #[crate_type="lib"];
 //TODO: when linked_hashmap enters libextra, replace this
 extern mod ord_hashmap;
+extern mod extra;
 
 use std::to_bytes::*;
+use extra::serialize::*;
 use ord_hashmap::*;
 
 static l_end: bool = true;
 
 pub trait BsonFormattable {
 	fn bson_doc_fmt(&self) -> Document;
+}
+
+pub struct BsonDocEncoder {
+	priv buf: ~[u8],
+	priv curr_key: ~str
 }
 
 #[deriving(Eq)]
@@ -57,7 +64,142 @@ pub struct BsonDocument {
 	fields: ~OrderedHashmap<~str, Document>
 }
 
+impl Encoder for BsonDocEncoder {
+	fn emit_nil(&mut self) { let key = self.key(); self.buf.push_all(key); }
+	fn emit_uint(&mut self, _: uint) { fail!("uint not implemented") }
+	fn emit_u8(&mut self, v: u8) { self.buf.push(v) }
+	fn emit_u16(&mut self, _: u16) { fail!("uint not implemented") }
+	fn emit_u32(&mut self, _: u32) { fail!("uint not implemented") }
+	fn emit_u64(&mut self, _: u64) { fail!("uint not implemented") }
+	fn emit_int(&mut self, _: int) { fail!("int not implemented") }
+	fn emit_i64(&mut self, v: i64) { let key = self.key(); self.buf.push_all(key + v.to_bytes(l_end)) }
+	fn emit_i32(&mut self, v: i32) { let key = self.key(); self.buf.push_all(key + v.to_bytes(l_end)) }
+	fn emit_i16(&mut self, _: i16) { fail!("i16 not implemented") }
+	fn emit_i8(&mut self, _: i8) { fail!("i8 not implemented") }
+	fn emit_bool(&mut self, v: bool) { let key = self.key(); self.buf.push_all(key + (if v {~[1]} else {~[0]})) }
+	fn emit_f64(&mut self, v: f64) {
+		let x: [u8,..8] = unsafe { std::cast::transmute(v) };
+		let key = self.key();
+		self.buf.push_all(key + x);
+	} 
+	fn emit_f32(&mut self, v: f32) { self.emit_f64(v as f64); }
+	fn emit_float(&mut self, v: float) { self.emit_f64(v as f64); }
+	fn emit_str(&mut self, v: &str) { let key = self.key(); self.buf.push_all(key + ((v.to_bytes(l_end) + [0]).len() as i32).to_bytes(l_end) + v.to_bytes(l_end) + [0]); }
+	
+	//embedded
+	fn emit_struct(&mut self, _: &str, _: uint, _: &fn(&mut BsonDocEncoder)) {
+		fail!("not implemented");
+	}
+	
+	//unimplemented junk
+	fn emit_char(&mut self, _: char) { fail!("not implemented") }
+	fn emit_enum(&mut self, _: &str, _: &fn(&mut BsonDocEncoder)) { fail!("not implemented") }
+	fn emit_enum_variant(&mut self, _: &str, _: uint, _:uint, _:&fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_enum_variant_arg(&mut self, _:uint, _:&fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_enum_struct_variant(&mut self, _: &str, _: uint, _:uint, _:&fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_enum_struct_variant_field(&mut self, _: &str, _:uint, _:&fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_struct_field(&mut self, _: &str, _:uint, _:&fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_tuple(&mut self, _: uint, _:&fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_tuple_arg(&mut self, _:uint, _:&fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_tuple_struct(&mut self, _: &str, _:uint, _:&fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_tuple_struct_arg(&mut self, _:uint, _:&fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_option(&mut self, _:&fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_option_none(&mut self) { fail!("not implemented")}
+	fn emit_option_some(&mut self, _: &fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_seq(&mut self, _: uint, _: &fn(&mut BsonDocEncoder)) { fail!("not implemented")}	
+	fn emit_seq_elt(&mut self, _: uint, _: &fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_map(&mut self, _: uint, _: &fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_map_elt_key(&mut self, _: uint, _: &fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+	fn emit_map_elt_val(&mut self, _: uint, _: &fn(&mut BsonDocEncoder)) { fail!("not implemented")}
+
+}
+
+impl BsonDocEncoder {
+	fn with_key<'a>(&'a mut self,key: ~str) -> &'a mut BsonDocEncoder{
+		self.curr_key = key;
+		self
+	}
+
+	fn key(&self) -> ~[u8] {
+		self.curr_key.to_bytes(l_end) + [0]
+	}
+
+	fn emit_size(&mut self, v: i32) {
+		self.buf.push_all(v.to_bytes(l_end));
+	}
+
+	fn emit_key(&mut self) {
+		let key = self.key();
+		self.buf.push_all(key);
+	}
+
+	fn emit_minkey(&mut self) {
+		let key = self.key();
+		self.buf.push_all(~[0xFF] + key);
+	}
+
+	fn emit_maxkey(&mut self) {
+		let key = self.key();
+		self.buf.push_all(~[0x7F] + key);
+	}
+
+	fn emit_oid(&mut self, v: &[u8]) {
+		if !(v.len() == 12) {
+			fail!(fmt!("invalid object id found: %?", v))
+		}
+		let key = self.key();
+		self.buf.push_all(key + v)
+	}
+
+	fn emit_cstr(&mut self, v: &str) {
+		self.buf.push_all(v.to_bytes(l_end) + [0]);
+	}
+
+	fn emit_regex(&mut self, v1: &str, v2: &str) {
+		let key = self.key();
+		self.buf.push_all(key + v1.to_bytes(l_end) + [0] + v2.to_bytes(l_end) + [0]);
+	}
+
+	fn new() -> BsonDocEncoder { BsonDocEncoder { buf: ~[], curr_key: ~"" } }
+}
+
+impl Encodable<BsonDocEncoder> for BsonDocument {
+	fn encode(&self, encoder: &mut BsonDocEncoder) {
+		encoder.emit_size(self.size);
+		for self.fields.each |&k,&v| {
+			match v {
+				Double(f) => { encoder.emit_u8(0x01); encoder.with_key(k).emit_f64(f as f64); }
+				UString(ref s) => { encoder.emit_u8(0x02); encoder.with_key(k).emit_str(*s); }
+				Embedded(ref doc) => { encoder.emit_u8(0x03); encoder.with_key(k).emit_key(); doc.encode(encoder); }
+				Array(ref doc) => { encoder.emit_u8(0x04); encoder.with_key(k).emit_key(); doc.encode(encoder); }
+				ObjectId(ref id) => { encoder.emit_u8(0x07); encoder.with_key(k).emit_oid(*id); }
+				Bool(b) => { encoder.emit_u8(0x08); encoder.with_key(k).emit_bool(b); }
+				UTCDate(i) => { encoder.emit_u8(0x09); encoder.with_key(k).emit_i64(i); }
+				Null => { encoder.emit_u8(0x0A); encoder.with_key(k).emit_nil(); }
+				Regex(ref s1, ref s2) => { encoder.emit_u8(0x0B); encoder.with_key(k).emit_regex(*s1, *s2); }
+				JScript(ref s) => { encoder.emit_u8(0x0D); encoder.with_key(k).emit_str(*s); }
+				JScriptWithScope(ref s, ref doc) => { 
+					encoder.emit_u8(0x0F); 
+					encoder.with_key(k).emit_i32(5 + doc.size + (s.to_bytes(l_end).len() as i32));
+					encoder.emit_cstr(*s); 
+					doc.encode(encoder);
+				}
+				Int32(i) => { encoder.emit_u8(0x10); encoder.with_key(k).emit_i32(i); }
+				Timestamp(i) => { encoder.emit_u8(0x11); encoder.with_key(k).emit_i64(i); }
+				Int64(i) => { encoder.emit_u8(0x12); encoder.with_key(k).emit_i64(i); }
+				_ => fail!("herp")
+			}
+		}
+		encoder.emit_u8(0);
+	}
+}
+
 impl<'self> BsonDocument {
+	pub fn to_bson(&self) -> ~[u8] {
+		let mut encoder = BsonDocEncoder::new();
+		self.encode(&mut encoder);
+		encoder.buf
+	}
 	pub fn contains_key(&self, key: ~str) -> bool {
 		self.fields.contains_key(&key)
 	}
