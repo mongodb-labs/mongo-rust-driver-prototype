5806b313cae6b6fade881fc555bd548404559a21
jchinlee <jaoke.chinlee@10gen.com>
2013-07-22 13:47:47 -0400

    Revert "read/write differentiation"
    This reverts commit 9b4a373ae3532ded7e7837aec0e936b265391ad7.

--- src/libmongo/conn_node.rs
+++ src/libmongo/conn_node.rs
@@ -33,6 +33,7 @@ pub struct NodeConnection {
     priv server_ip_str : ~str,
     priv server_port : uint,
     priv server_ip : Cell<IpAddr>,
+    priv iotask : iotask::IoTask,
     priv sock : Cell<@Socket>,
     //priv port : @mut Option<@Port<Result<~[u8], TcpErrData>>>,
     priv port : Cell<@GenericPort<PortResult>>,
@@ -62,7 +63,7 @@ impl Connection for NodeConnection {
 
         // set up the socket --- for now, just v4
         // XXX
-        let sock = match connect(ip, self.server_port, &global_loop::get()) {
+        let sock = match connect(ip, self.server_port, &self.iotask) {
             Err(GenericConnectErr(ename, emsg)) => return Err(MongoErr::new(~"conn::connect", ename, emsg)),
             Err(ConnectionRefused) => return Err(MongoErr::new(~"conn::connect", ~"EHOSTNOTFOUND", ~"Invalid IP or port")),
             Ok(sock) => @sock as @Socket
@@ -190,6 +191,7 @@ impl NodeConnection {
             server_ip_str : server_ip_str,
             server_port : server_port,
             server_ip : Cell::new_empty(),
+            iotask : global_loop::get().clone(),
             sock : Cell::new_empty(),
             port : Cell::new_empty(),
             ping : Cell::new_empty(),
