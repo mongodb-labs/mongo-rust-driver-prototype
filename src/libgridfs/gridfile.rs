2b8af6ec7f9cb1ceb13ed1d36945f609c65f01ab
Jed Estep <aje@jhu.edu>
2013-07-22 11:36:15 -0400

    imports

--- src/libgridfs/gridfile.rs
+++ src/libgridfs/gridfile.rs
@@ -1,3 +1,19 @@
 use bson::encode::*;
 
 use mongo::coll::*;
+
+pub struct GridIn {
+    collection: @Collection,
+    closed: bool
+}
+
+impl GridIn {
+    pub fn new(coll: @Collection) -> GridIn {
+        GridIn {
+            collection: coll,
+            closed: false
+        }
+    }
+
+
+}
