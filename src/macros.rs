0ac908fa519d28731c4b87d59441b9a2c9522e47
Saghm Rossi <saghmrossi@gmail.com>
2015-06-17 16:40:54 -0400

    added macros for making BSON documents

---
+++ src/macros.rs
@@ -0,0 +1,36 @@
+#[macro_export]
+macro_rules! doc {
+    ( $( $k:expr => $v: expr),* ) => {
+        {
+            let mut doc = Document::new();
+            $(
+                doc.insert($k.to_owned(), $v);
+            )*
+            doc
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! nested_doc {
+    ( $( $k:expr => $v: expr),* ) => {
+        Bson::Document(doc!(
+            $( $k => $v),*
+        ))
+    }
+}
+
+// Example for future documentation use
+//
+// ```
+// doc!(
+//     "_id" => Bson::I32(1),
+//     "x" => Bson::I32(11),
+//     "$filter" => nested_doc!(
+//         "_id" => nested_doc!(
+//             "$gt" => 1,
+//             "$lt" => 6
+//         )
+//     )
+// )
+// ```
