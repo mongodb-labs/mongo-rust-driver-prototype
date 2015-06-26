f12f4b19d8a64bdc54b1abd36dcb06fadf7baa80
Kevin Yeh <kevinyeah@utexas.edu>
2015-06-26 16:33:31 -0400

    Convert to ObjectId struct; fix endian-ness and string conversions

--- tests/client/oid.rs
+++ tests/client/oid.rs
@@ -1,4 +1,4 @@
-use mongodb::client::oid;
+use mongodb::client::oid::ObjectId;
 use rustc_serialize::hex::ToHex;
 
 #[test]
@@ -18,56 +18,81 @@ fn deserialize() {
         0x33u8,  // increment is 1122867
         ];
 
-    assert_eq!(3735928559 as u32, oid::get_timestamp(bytes));
-    assert_eq!(11259375 as u32, oid::get_machine_id(bytes));
-    assert_eq!(10746 as u16, oid::get_pid(bytes));
-    assert_eq!(1122867 as u32, oid::get_counter(bytes));
+    let oid = ObjectId::with_bytes(bytes);
+    assert_eq!(3735928559 as u32, oid.timestamp());
+    assert_eq!(11259375 as u32, oid.machine_id());
+    assert_eq!(10746 as u16, oid.process_id());
+    assert_eq!(1122867 as u32, oid.counter());
 }
 
 #[test]
 fn timestamp() {
     let time: u32 = 2000000;
-    let oid = oid::with_timestamp(time);
-    let timestamp = oid::get_timestamp(oid);
+    let oid = ObjectId::with_timestamp(time);
+    let timestamp = oid.timestamp();
     assert_eq!(time, timestamp);
 }
 
 #[test]
 fn timestamp_is_big_endian() {
     let time: u32 = 3857379;
-    let oid = oid::with_timestamp(time);
-    assert_eq!(0x00u8, oid[0]);
-    assert_eq!(0x3Au8, oid[1]);
-    assert_eq!(0xDBu8, oid[2]);
-    assert_eq!(0xE3u8, oid[3]);
+    let oid = ObjectId::with_timestamp(time);
+    assert_eq!(0x00u8, oid.bytes()[0]);
+    assert_eq!(0x3Au8, oid.bytes()[1]);
+    assert_eq!(0xDBu8, oid.bytes()[2]);
+    assert_eq!(0xE3u8, oid.bytes()[3]);
 }
 
 #[test]
 fn string_oid() {
     let s = "123456789012123456789012";
-    let oid_res = oid::with_string(s);
+    let oid_res = ObjectId::with_string(s);
     assert!(oid_res.is_ok());
-    let actual_s = oid_res.unwrap().to_hex();
+    let actual_s = oid_res.unwrap().bytes().to_hex();
     assert_eq!(s.to_owned(), actual_s);
 }
 
 #[test]
 fn byte_string_oid() {
     let s = "541b1a00e8a23afa832b218e";
-    let oid_res = oid::with_string(s);
+    let oid_res = ObjectId::with_string(s);
     assert!(oid_res.is_ok());
     let oid = oid_res.unwrap();
     let bytes: [u8; 12] = [0x54u8, 0x1Bu8, 0x1Au8, 0x00u8,
                            0xE8u8, 0xA2u8, 0x3Au8, 0xFAu8,
                            0x83u8, 0x2Bu8, 0x21u8, 0x8Eu8];
 
-    assert_eq!(bytes, oid);
+    assert_eq!(bytes, oid.bytes());
 }
 
 #[test]
 fn oid_equals() {
-    let oid_res = oid::generate();
+    let oid_res = ObjectId::new();
     assert!(oid_res.is_ok());
     let oid = oid_res.unwrap();
     assert_eq!(oid, oid);
+}
+
+#[test]
+fn oid_not_equals() {
+    let oid1_res = ObjectId::new();
+    let oid2_res = ObjectId::new();
+    assert!(oid1_res.is_ok());
+    assert!(oid2_res.is_ok());
+
+    let oid1 = oid1_res.unwrap();
+    let oid2 = oid2_res.unwrap();
+    assert!(oid1 != oid2);
+}
+
+#[test]
+fn increasing() {
+    let oid1_res = ObjectId::new();
+    let oid2_res = ObjectId::new();
+    assert!(oid1_res.is_ok());
+    assert!(oid2_res.is_ok());
+
+    let oid1 = oid1_res.unwrap();
+    let oid2 = oid2_res.unwrap();
+    assert!(oid1 < oid2);
 }
