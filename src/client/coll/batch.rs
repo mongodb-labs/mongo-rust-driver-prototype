5ac5b096531caec9f4eff8fee12c87e807e8a429
Kevin Yeh <kevinyeah@utexas.edu>
2015-07-01 13:29:07 -0400

    Bulk delete and update ops

--- src/client/coll/batch.rs
+++ src/client/coll/batch.rs
@@ -2,16 +2,36 @@ use bson::Document;
 use client::coll::options::WriteModel;
 use client::coll::results::InsertManyResult;
 
-struct UpdateModel {
+pub struct UpdateModel {
     pub filter: Document,
     pub update: Document,
     pub upsert: bool,
     pub multi: bool,
 }
 
-struct DeleteModel {
+pub struct DeleteModel {
     pub filter: Document,
     pub multi: bool,
+}
+
+impl UpdateModel {
+    pub fn new(filter: Document, update: Document, upsert: bool, multi: bool) -> UpdateModel {
+        UpdateModel {
+            filter: filter,
+            update: update,
+            upsert: upsert,
+            multi: multi,
+        }
+    }
+}
+
+impl DeleteModel {
+    pub fn new(filter: Document, multi: bool) -> DeleteModel {
+        DeleteModel {
+            filter: filter,
+            multi: multi,
+        }
+    }
 }
 
 pub enum Batch {
