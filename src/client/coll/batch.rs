20dca2c86d44e135601d43947e78887608a2847e
Saghm Rossi <saghmrossi@gmail.com>
2015-07-02 16:07:58 -0400

    added the rest of the bulk write functionality and a test for ordered bulk writes

--- src/client/coll/batch.rs
+++ src/client/coll/batch.rs
@@ -1,17 +1,20 @@
 use bson::Document;
 use client::coll::options::WriteModel;
-use client::coll::results::InsertManyResult;
+use std::convert::From;
 
+#[derive(Debug)]
+pub struct DeleteModel {
+    pub filter: Document,
+    pub multi: bool,
+}
+
+#[derive(Debug)]
 pub struct UpdateModel {
     pub filter: Document,
     pub update: Document,
     pub upsert: bool,
     pub multi: bool,
-}
-
-pub struct DeleteModel {
-    pub filter: Document,
-    pub multi: bool,
+    pub is_replace: bool,
 }
 
 impl UpdateModel {
@@ -21,6 +24,7 @@ impl UpdateModel {
             update: update,
             upsert: upsert,
             multi: multi,
+            is_replace: false,
         }
     }
 }
@@ -34,8 +38,91 @@ impl DeleteModel {
     }
 }
 
+#[derive(Debug)]
 pub enum Batch {
-    Insert {
-        documents: Vec<Document>,
+    Insert(Vec<Document>),
+    Delete(Vec<DeleteModel>),
+    Update(Vec<UpdateModel>),
+}
+
+impl From<WriteModel> for Batch {
+    fn from(model: WriteModel) -> Batch {
+        match model {
+            WriteModel::InsertOne { document } => Batch::Insert(vec![document]),
+            WriteModel::DeleteOne { filter } =>
+                Batch::Delete(vec![DeleteModel { filter: filter, multi: false }]),
+            WriteModel::DeleteMany { filter } =>
+                Batch::Delete(vec![DeleteModel { filter: filter, multi: true }]),
+            WriteModel::ReplaceOne { filter, replacement: update, upsert } =>
+                Batch::Update(vec![UpdateModel { filter: filter,
+                                                 update: update,
+                                                 upsert: upsert, multi: false,
+                                                 is_replace: true }]),
+            WriteModel::UpdateOne { filter, update, upsert } =>
+                Batch::Update(vec![UpdateModel { filter: filter,
+                                                 update: update,
+                                                 upsert: upsert, multi: false,
+                                                 is_replace: false }]),
+            WriteModel::UpdateMany { filter, update, upsert } =>
+                Batch::Update(vec![UpdateModel { filter: filter,
+                                                 update: update,
+                                                 upsert: upsert, multi: true,
+                                                 is_replace: false }]),
+        }
+    }
+}
+
+impl Batch {
+    pub fn len(&self) -> i64 {
+        let length = match self {
+            &Batch::Insert(ref v) => v.len(),
+            &Batch::Delete(ref v) => v.len(),
+            &Batch::Update(ref v) => v.len(),
+        };
+
+        length as i64
+    }
+
+    pub fn merge_model(&mut self, model: WriteModel) -> Option<WriteModel> {
+        match self {
+            &mut Batch::Insert(ref mut docs) => {
+                match model {
+                    WriteModel::InsertOne { document } => docs.push(document),
+                    _ => return Some(model)
+                }
+            },
+            &mut Batch::Delete(ref mut models) => {
+                match model {
+                    WriteModel::DeleteOne { filter} =>
+                        models.push(DeleteModel { filter: filter, multi: false}),
+                    WriteModel::DeleteMany { filter } =>
+                        models.push(DeleteModel { filter: filter, multi: true}),
+                    _ => return Some(model)
+                }
+            },
+            &mut Batch::Update(ref mut models) => {
+                match model {
+                    WriteModel::ReplaceOne { filter, replacement: update,
+                                             upsert } =>
+                        models.push(UpdateModel { filter: filter,
+                                                  update: update,
+                                                  upsert: upsert, multi: false,
+                                                  is_replace: true }),
+                    WriteModel::UpdateOne { filter, update, upsert } =>
+                        models.push(UpdateModel { filter: filter,
+                                                  update: update,
+                                                  upsert: upsert, multi: false,
+                                                  is_replace: false }),
+                    WriteModel::UpdateMany { filter, update, upsert } =>
+                        models.push(UpdateModel { filter: filter,
+                                                  update: update,
+                                                  upsert: upsert, multi: true,
+                                                  is_replace: false }),
+                    _ => return Some(model)
+                }
+            }
+        }
+
+        None
     }
 }
--- tests/client/bulk.rs
+++ tests/client/bulk.rs
@@ -71,3 +71,142 @@ fn bulk_unordered_insert_only() {
         }
     }
 }
+
+#[test]
+fn bulk_ordered_mix() {
+    let models = vec![
+        WriteModel::InsertOne { document: doc! {
+            "_id" => (1),
+            "x" => (11)
+        }},
+        WriteModel::InsertOne { document: doc! {
+            "_id" => (2),
+            "x" => (22)
+        }},
+        WriteModel::InsertOne { document: doc! {
+            "_id" => (3),
+            "x" => (33)
+        }},
+        WriteModel::InsertOne { document: doc! {
+            "_id" => (4),
+            "x" => (44)
+        }},
+        WriteModel::ReplaceOne {
+            filter: doc! { "_id" => (3) },
+            replacement: doc! { "x" => (37) },
+            upsert: true,
+        },
+        WriteModel::UpdateMany {
+            filter: doc! { "_id" => { "$lt" => (3) } },
+            update: doc! { "$inc" => { "x" => (1) } },
+            upsert: false,
+        },
+        WriteModel::DeleteOne { filter: doc! {
+            "_id" => (4)
+        }},
+        WriteModel::InsertOne { document: doc! {
+            "_id" => (5),
+            "x" => (55)
+        }},
+        WriteModel::UpdateOne {
+            filter: doc! { "_id" => (6) },
+            update: doc! { "$set" =>  { "x" => (62) } },
+            upsert: true
+        },
+        WriteModel::InsertOne { document: doc! {
+            "_id" => (101),
+            "x" => ("dalmations")
+        }},
+        WriteModel::InsertOne { document: doc! {
+            "_id" => (102),
+            "x" => ("strawberries")
+        }},
+        WriteModel::InsertOne { document: doc! {
+            "_id" => (103),
+            "x" => ("blueberries")
+        }},
+        WriteModel::InsertOne { document: doc! {
+            "_id" => (104),
+            "x" => ("bananas")
+        }},
+        WriteModel::DeleteMany { filter: doc! {
+            "_id" => { "$gte" => (103) }
+        }},
+    ];
+
+    let client = MongoClient::new("localhost", 27017).unwrap();
+    let db = client.db("test");
+    let coll = db.collection("bulk_ordered_mix");
+
+    coll.drop().unwrap();
+
+    let result = coll.bulk_write(models, true);
+
+    assert_eq!(result.inserted_count, 9);
+    assert_eq!(result.inserted_ids.len() as i32, result.inserted_count);
+    assert_eq!(result.matched_count, 3);
+    assert_eq!(result.modified_count, 3);
+    assert_eq!(result.deleted_count, 3);
+    assert_eq!(result.upserted_count, 1);
+    assert_eq!(result.upserted_ids.len() as i32, result.upserted_count);
+
+    match result.inserted_ids.get(&0).unwrap() {
+        &Bson::I32(1) => (),
+        &Bson::I64(1) => (),
+        id => panic!("Invalid inserted id at index 0: {:?}", id)
+    }
+
+    match result.inserted_ids.get(&1).unwrap() {
+        &Bson::I32(2) => (),
+        &Bson::I64(2) => (),
+        id => panic!("Invalid inserted id at index 1: {:?}", id)
+    }
+
+    match result.inserted_ids.get(&2).unwrap() {
+        &Bson::I32(3) => (),
+        &Bson::I64(3) => (),
+        id => panic!("Invalid inserted id at index 2: {:?}", id)
+    }
+
+    match result.inserted_ids.get(&3).unwrap() {
+        &Bson::I32(4) => (),
+        &Bson::I64(4) => (),
+        id => panic!("Invalid inserted id at index 3: {:?}", id)
+    }
+
+    match result.inserted_ids.get(&7).unwrap() {
+        &Bson::I32(5) => (),
+        &Bson::I64(5) => (),
+        id => panic!("Invalid inserted id at index 7: {:?}", id)
+    }
+
+    match result.inserted_ids.get(&9).unwrap() {
+        &Bson::I32(101) => (),
+        &Bson::I64(101) => (),
+        id => panic!("Invalid inserted id at index 9: {:?}", id)
+    }
+
+    match result.inserted_ids.get(&10).unwrap() {
+        &Bson::I32(102) => (),
+        &Bson::I64(102) => (),
+        id => panic!("Invalid inserted id at index 10: {:?}", id)
+    }
+
+    match result.inserted_ids.get(&11).unwrap() {
+        &Bson::I32(103) => (),
+        &Bson::I64(103) => (),
+        id => panic!("Invalid inserted id at index 11: {:?}", id)
+    }
+
+    match result.inserted_ids.get(&12).unwrap() {
+        &Bson::I32(104) => (),
+        &Bson::I64(104) => (),
+        id => panic!("Invalid inserted id at index 12: {:?}", id)
+    }
+
+    match result.upserted_ids.get(&8).unwrap() {
+        &Bson::I32(6) => (),
+        &Bson::I64(6) => (),
+        id => panic!("Invalid inserted id at index 8: {:?}", id)
+    }
+}
