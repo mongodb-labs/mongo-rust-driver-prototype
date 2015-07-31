07629172a5a869a8a1b7826dfc87da2c66ab9598
Saghm Rossi <saghmrossi@gmail.com>
2015-07-31 13:48:50 -0400

    added test for logging

--- tests/apm/mod.rs
+++ tests/apm/mod.rs
@@ -1,3 +1,6 @@
+use std::fs::{self, File};
+use std::io::{BufRead, BufReader};
+
 use bson::Bson;
 use mongodb::{Client, CommandResult, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
@@ -32,4 +35,85 @@ fn command_duration() {
     };
 
     coll.find(Some(doc), None).unwrap();
+}
+
+#[test]
+fn logging() {
+    fs::remove_file("test_log.txt").unwrap();
+    let client = Client::connect_with_log_file("localhost", 27017, "test_log.txt").unwrap();
+    let db = client.db("test");
+    let coll = db.collection("logging");
+    coll.drop().unwrap();
+
+    let doc1 = doc! { "_id" => 1 };
+    let doc2 = doc! { "_id" => 2 };
+    let doc3 = doc! { "_id" => 3 };
+
+    coll.insert_one(doc1, None).unwrap();
+    coll.insert_one(doc2, None).unwrap();
+    coll.insert_one(doc3, None).unwrap();
+
+    let filter = doc! {
+        "_id" => { "$gt" => 1 }
+    };
+
+    coll.find(Some(filter), None).unwrap();
+
+    let f = File::open("test_log.txt").unwrap();
+    let mut file = BufReader::new(&f);
+    let mut line = String::new();
+
+    // Drop collection started
+    file.read_line(&mut line).unwrap();
+    assert_eq!("COMMAND.drop_collection 127.0.0.1:27017 STARTED: { drop: \"logging\" }\n", &line);
+
+    // Drop collection completed
+    line.clear();
+    file.read_line(&mut line).unwrap();
+    assert!(line.starts_with("COMMAND.drop_collection 127.0.0.1:27017 COMPLETED: { ns: \"test.logging\", nIndexesWas: 1, ok: 1 } ("));
+    assert!(line.ends_with(" ns)\n"));
+
+    // First insert started
+    line.clear();
+    file.read_line(&mut line).unwrap();
+    assert_eq!("COMMAND.insert_one 127.0.0.1:27017 STARTED: { insert: \"logging\", documents: [{ _id: 1 }], ordered: true, writeConcern: { w: 1, wtimeout: 0, j: false } }\n", &line);
+
+    // First insert completed
+    line.clear();
+    file.read_line(&mut line).unwrap();
+    assert!(line.starts_with("COMMAND.insert_one 127.0.0.1:27017 COMPLETED: { ok: 1, n: 1 } ("));
+    assert!(line.ends_with(" ns)\n"));
+
+    // Second insert started
+    line.clear();
+    file.read_line(&mut line).unwrap();
+    assert_eq!("COMMAND.insert_one 127.0.0.1:27017 STARTED: { insert: \"logging\", documents: [{ _id: 2 }], ordered: true, writeConcern: { w: 1, wtimeout: 0, j: false } }\n", &line);
+
+    // Second insert completed
+    line.clear();
+    file.read_line(&mut line).unwrap();
+    assert!(line.starts_with("COMMAND.insert_one 127.0.0.1:27017 COMPLETED: { ok: 1, n: 1 } ("));
+    assert!(line.ends_with(" ns)\n"));
+
+    // Third insert started
+    line.clear();
+    file.read_line(&mut line).unwrap();
+    assert_eq!("COMMAND.insert_one 127.0.0.1:27017 STARTED: { insert: \"logging\", documents: [{ _id: 3 }], ordered: true, writeConcern: { w: 1, wtimeout: 0, j: false } }\n", &line);
+
+    // Third insert completed
+    line.clear();
+    file.read_line(&mut line).unwrap();
+    assert!(line.starts_with("COMMAND.insert_one 127.0.0.1:27017 COMPLETED: { ok: 1, n: 1 } ("));
+    assert!(line.ends_with(" ns)\n"));
+
+    // Find command started
+    line.clear();
+    file.read_line(&mut line).unwrap();
+    assert_eq!("COMMAND.find 127.0.0.1:27017 STARTED: { find: \"logging\", filter: {  }, projection: {  }, skip: 0, limit: 0, batchSize: 20, sort: {  } }\n", &line);
+
+    // Find command completed
+    line.clear();
+    file.read_line(&mut line).unwrap();
+    assert!(line.starts_with("COMMAND.find 127.0.0.1:27017 COMPLETED: { cursor: { id: 0, ns: \"test.logging\", firstBatch: [{ _id: 2 }, { _id: 3 }] }, ok: 1 } ("));
+    assert!(line.ends_with(" ns)\n"));
 }
